dd37a04d89ad2fafb9d3a6a0e89a86e9242c454e
winbiologist <winbiologist@ucla.edu>
2017-09-22 14:01:45 -0700

    Bootcamp Exercise 1 (Part1: exercise 1-3)

---
+++ Bootcamp Exercise 1 (part1_1 to 3)_ Michael A.R
@@ -0,0 +1,36 @@
+
+#Exercise1 #DONE
+
+for (x in 1:9)
+{
+cat("\n")
+if (x == 9) 
+cat("*")
+}
+
+#################################################################################
+#Exercise2 #Done
+for (x in 1:10) {
+  x <= 10
+  cat("*&")
+}
+#################################################################################
+#Exercise3 
+#1
+#initial values is 10
+#start values is 11
+#end values is 15
+
+#2
+#initial values is 0
+#start values is -4
+#end values is -30
+
+#3
+#initial values is 12
+#start values is -1
+#end values is -4
+
+#################################################################################
+
+
